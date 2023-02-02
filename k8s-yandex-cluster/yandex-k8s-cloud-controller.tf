b9701d2e8b5cd177a935509d8f0456e7c757a724
Dobry-kot <dlputilin@dobry-kot.ru>
2023-02-02 17:08:58 +0300

    [main] рефакторинг

--- k8s-yandex-cluster/yandex-k8s-cloud-controller.tf
+++ k8s-yandex-cluster/yandex-k8s-cloud-controller.tf
@@ -1,16 +1,13 @@
 module "k8s-yandex-cloud-controller" {
   source = "git::https://github.com/fraima/terraform-modules//modules/k8s-yandex-cloud-controller?ref=main"
 
+  chart_version = "0.0.3"
+
   yandex_default_vpc_name         = var.master_group.vpc_name
   yandex_default_route_table_name = var.master_group.route_table_name
 
-  k8s_api_server      = local.kube_apiserver_ip
-  k8s_api_server_port = local.global_vars.kubernetes-ports.kube-apiserver-port-lb
-
-  pod_cidr      = local.global_vars.k8s_network.pod_cidr
-
-  cluster_name  = local.global_vars.cluster_metadata.cluster_name
-
-  chart_version = "0.0.3"
+  global_vars         = module.k8s-yandex-cluster.k8s_global_vars
+  
+  extra_values        = {} 
 
 }
